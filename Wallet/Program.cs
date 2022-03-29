cc01a236ccb58a912366759dc765fd9b883e960f
Harold Pedraza <t1harole@hotmail.com>
2022-03-29 14:16:54 -0500

    server and client

---
+++ Wallet/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace Wallet
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Harold" };
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
+                true
+            );
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine($"Rspuesta: {reply.Message}");
+            Console.ReadLine();
+        }
+    }
+}
